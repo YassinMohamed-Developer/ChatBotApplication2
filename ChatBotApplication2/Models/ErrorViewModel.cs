0c804ceff8ffcb19c853c2de61253f07e94fdc4e
yassin <yassinmohamed.cs@gmil.com>
2026-08-03 18:13:19 +0300

    Add project files.

---
+++ ChatBotApplication2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatBotApplication2.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
